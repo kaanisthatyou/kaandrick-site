c761ff5fc4e585135ff507b2d0d7a80267cc49d9
Bearism <kaan12gs@gmail.com>
2025-08-16 19:29:10 +0300

    Fixed case-sensitive importing error

--- src/components/ui/Aurora.jsx
+++ src/components/ui/Aurora.jsx
@@ -1,7 +1,7 @@
 import { Renderer, Program, Mesh, Color, Triangle } from "ogl";
 import { useEffect, useRef } from "react";
 
-import './Aurora.css';
+import './aurora.css';
 
 const VERT = `#version 300 es
 in vec2 position;
